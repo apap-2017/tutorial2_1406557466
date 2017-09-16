859ebb62e7683b185d8adb9f132ce323bc01a5f9
emmasharahwati <emmasharahh@gmail.com>
2017-09-16 12:35:26 +0700

    First commit

---
+++ src/main/java/com/example/CobaApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobaApplication.class, args);
+	}
+}
